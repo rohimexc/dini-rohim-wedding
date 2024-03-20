da583d700ab520049a83318291e57a0ffda009a5
rohimexc <rohimexcellent5918@gmail.com>
2024-03-21 03:05:20 +0800

    get data

---
+++ data.php
@@ -0,0 +1,23 @@
+<?php
+$servername = "localhost";
+$username = "root"; // Sesuaikan dengan username MySQL Anda
+$password = ""; // Sesuaikan dengan password MySQL Anda
+$dbname = "dini-rohim-wedding";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT * FROM form_data";
+$result = $conn->query($sql);
+
+$data = array();
+if ($result->num_rows > 0) {
+    while($row = $result->fetch_assoc()) {
+        $data[] = $row;
+    }
+}
+
+$conn->close();
+?>
